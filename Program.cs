fa1ba2bf30d0ed2c40cbdcd84b0f38d7a45c43fd
falau <falau@LENOVO>
2024-10-31 20:04:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Set1Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n>0)
+            {
+                Console.WriteLine(n%10);
+                n /= 10;
+            }
+        }
+    }
+}
